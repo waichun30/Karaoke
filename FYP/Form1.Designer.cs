e9e985c5343918fbda8102eb52429bebb49093ff
waichun30 <40302159+waichun30@users.noreply.github.com>
2018-09-06 01:46:06 +0800

    source

---
+++ FYP/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace FYP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Categories = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Favorite = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Artist = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Duration = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Rating = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Categories});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(310, 1009);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Categories
+            // 
+            this.Categories.HeaderText = "Categories";
+            this.Categories.Name = "Categories";
+            this.Categories.Width = 1000;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Favorite,
+            this.Name,
+            this.Artist,
+            this.Duration,
+            this.Rating});
+            this.dataGridView2.Location = new System.Drawing.Point(328, 654);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(1580, 367);
+            this.dataGridView2.TabIndex = 1;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // Favorite
+            // 
+            this.Favorite.HeaderText = "Favorite";
+            this.Favorite.Name = "Favorite";
+            // 
+            // Name
+            // 
+            this.Name.HeaderText = "Name";
+            this.Name.Name = "Name";
+            this.Name.Width = 500;
+            // 
+            // Artist
+            // 
+            this.Artist.HeaderText = "Artist";
+            this.Artist.Name = "Artist";
+            this.Artist.Width = 400;
+            // 
+            // Duration
+            // 
+            this.Duration.HeaderText = "Duration";
+            this.Duration.Name = "Duration";
+            this.Duration.Width = 300;
+            // 
+            // Rating
+            // 
+            this.Rating.HeaderText = "Rating";
+            this.Rating.Name = "Rating";
+            this.Rating.Width = 300;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(354, 129);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(439, 357);
+            this.axWindowsMediaPlayer1.TabIndex = 2;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(438, 611);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(96, 21);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "With Vocal";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(753, 611);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(116, 21);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Without Vocal";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(460, 564);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            //this.Name = "Form1";
+            this.Text = "Sing Loud";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Categories;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Favorite;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Artist;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Duration;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Rating;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ FYP/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace FYP
+{
+    public partial class Form1 : Form
+    {
+        //
+        // Golbal variable since no well organize database
+        string Gfolder;
+        string Gsong;
+        bool play =false;
+        WMPLib.WindowsMediaPlayer Player = new WMPLib.WindowsMediaPlayer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            //getSongName("Hits");
+            //getDuration("Sia - Chandelier (Karaoke Version).mp4");
+            //fillSong("Hits");
+            fillCategories();
+            dataGridView1_CellContentClick(dataGridView2, new DataGridViewCellEventArgs(0, 0));
+            axWindowsMediaPlayer1.uiMode = "none";
+        }
+
+
+        // when click the catagery , work with fillSong function
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView2.Rows.Clear();
+            int index = e.RowIndex;
+            try
+            {
+                string s = dataGridView1.Rows[index].Cells[0].Value.ToString();
+                if (!fillSong(s))
+                    MessageBox.Show("No Song Found");
+                Gfolder = s;
+            }catch(Exception ex)
+            {
+
+            }
+            
+        }
+
+        // click song, work with playVocal function
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = e.RowIndex;
+
+            try
+            {
+                string song = dataGridView2.Rows[index].Cells[1].Value.ToString();
+                Gsong = song;
+                string c = Directory.GetCurrentDirectory().ToString() + @"\Music\" + Gfolder+@"\"+@song;
+                axWindowsMediaPlayer1.URL = @c;
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                playVocal(song.Replace(".mp4",""));
+                radioButton1.Checked = true;
+                button1.Text = "Pause";
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        // all song name in catagery
+        private List<string> getSongName(string folder)
+        {
+            List<string> song;
+            try {
+                song = new List<string>();
+
+                //Get curent Directory
+                string c = Directory.GetCurrentDirectory().ToString()+@"\Music\"+@folder;
+
+                // Go to Folder
+                DirectoryInfo d = new DirectoryInfo(c);
+
+                // Get Extension "mp4, mp3 ....
+                FileInfo[] files = d.GetFiles("*.mp4");
+
+
+                // store to array
+                foreach (FileInfo f in files)
+                    song.Add(f.Name);
+                return song;
+
+            }catch (Exception ex)
+            {
+                song = new List<string>();
+                string fail = "Fail to get song name.";
+                MessageBox.Show(fail);
+                return song;
+            }
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void playVideo()
+        {
+
+        }
+
+        private string getDuration(string folder,string song)
+        {
+            try
+            {
+                var player = new WindowsMediaPlayer();
+
+                string c = Directory.GetCurrentDirectory().ToString() +@"\Music\"+ @folder+@"\"+@song;
+                IWMPMedia a = player.newMedia(@c);
+                return TimeSpan.FromSeconds(a.duration).ToString();
+            }
+            catch(Exception ex)
+            {
+                string ms = "Fail to get duration for song: " + song;
+                MessageBox.Show(ms);
+                return "";
+            }
+        }
+        
+        // fill the datagridview with all song name
+        private bool fillSong(string cat)
+        {
+            try{
+                List<string> song = new List<string>();
+                song = getSongName(cat);
+                if (song.Count == 0)
+                    return false;
+                
+                for (int i = 0; i < song.Count ; i++)
+                {
+                    dataGridView2.Rows.Add(1);
+                    dataGridView2.Rows[i].Cells[1].Value = song[i];
+                    dataGridView2.Rows[i].Cells[2].Value = getDuration(cat,song[i]);
+                }
+                return true;
+            }catch(Exception ex){
+                return false;
+            }
+        }
+
+
+        // Get all category in List
+        private List<string> getFolderName()
+        {
+            List<string> folderName=new List<string>();
+            try
+            {
+                string d = Directory.GetCurrentDirectory().ToString() + @"\Music\";
+                string[] folders = Directory.GetDirectories(@d, "*", System.IO.SearchOption.AllDirectories);
+
+                DirectoryInfo d1 = new DirectoryInfo(d);
+                FileInfo[] files = d1.GetFiles("*");
+                foreach (string name in folders)
+                {
+                    folderName.Add(name.Replace(@d,""));
+                }
+                return folderName;
+            }
+            catch(Exception ex)
+            {
+                return folderName;
+            }
+        }
+
+        // Fill the Categories with List
+        private bool fillCategories()
+        {
+            List<string> folderName = new List<string>();
+            try
+            {
+                folderName = getFolderName();
+
+                for(int i = 0; i < folderName.Count; i++)
+                {
+                    dataGridView1.Rows.Add(1);
+                    dataGridView1.Rows[i].Cells[0].Value = folderName[i];
+                }
+
+                return true;
+            }
+            catch
+            {
+
+                return false;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.settings.mute = true;
+            Player.settings.mute = false;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.settings.mute = false;
+            Player.settings.mute = true;
+        }
+
+        //play vocal
+        private void playVocal(string song)
+        {
+            
+            song += ".mp3";
+            string c = Directory.GetCurrentDirectory().ToString() + @"\Music\" + @Gfolder+@"\"+@song;
+
+            Player.URL = @c;
+
+            Player.controls.play();
+            Console.Write(c);
+
+        }
+
+        private void songDatabase()
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            playSong();
+        }
+
+        private bool playSong()
+        {
+            try
+            {
+                if (play)
+                {
+                    Player.controls.play();
+                    axWindowsMediaPlayer1.Ctlcontrols.play();
+                    button1.Text = "Pause";
+                    play = false;
+                }
+                else
+                {
+                    Player.controls.pause();
+                    axWindowsMediaPlayer1.Ctlcontrols.pause();
+                    button1.Text = "Play";
+                    play = true;
+                }
+
+                return true;
+            }catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+    }
+}
